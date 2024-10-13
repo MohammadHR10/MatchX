bdb5ddadf20de7b74d30a69c0f4c804e2269886f
Avirup Bhattacharjee <avirup.b26@gmail.com>
2024-10-13 07:48:21 -0500

    fixed home page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Profile from './pages/Profile'; // Adjust the path as per your project structure
+import AppRoutes from './routes';
+//import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+//import Profile from './pages/Profile'; // Adjust the path as per your project structure
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    
       <div className="App">
-        <Routes>
-          {/* The "element" prop is used instead of "component" */}
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/" element={<h1>Home Page</h1>} />
-        </Routes>
+         <AppRoutes />
       </div>
-    </Router>
+    
   );
 }
 
